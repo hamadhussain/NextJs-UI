2bd86c1a78e2dd09775d6d8797ccdbbba499f696
Hammad Hussain <108784635+hamadhussain@users.noreply.github.com>
2025-04-11 20:33:41 +0500

    Update page.jsx

--- src/app/Criteria/page.jsx
+++ src/app/Criteria/page.jsx
@@ -207,7 +207,7 @@ export default function Home() {
           <GoSearch />
         </div>
         <div className="pl-4">
-          <button className="button px-3 py-1 text-sm">
+          <button className="addButton px-3 py-1 text-sm">
             ADD
           </button>
         </div>
