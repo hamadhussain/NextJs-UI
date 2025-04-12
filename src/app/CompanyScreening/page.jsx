08e6a454bc1290977556b505f3f9637c6a327762
Hammad Hussain <108784635+hamadhussain@users.noreply.github.com>
2025-04-12 23:27:26 +0500

    Update page.jsx

--- src/app/CompanyScreening/page.jsx
+++ src/app/CompanyScreening/page.jsx
@@ -6,7 +6,7 @@ const Companies = () => {
     <div className="flex justify-between py-3 px-8 items-center borderColor">
       <p className="text-lg ">Company Screening</p>
       <div className="flex  justify-center items-center gap-2 relative -left-3">
-        <button className="h-10 px-62 text-sm border-4 bg-white relative -left-8 text-black rounded-lg">
+        <button className="h-10 px-61.5 text-sm border-4 bg-white relative -left-8 text-black rounded-lg">
           Chat ExM
         </button>
         <div className="flex justify-center items-center gap-4 mr-24 relative ">
