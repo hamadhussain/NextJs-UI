961d6ef954b95a5cfacf32ff583dec9bcc9c0fc6
Hammad Hussain <108784635+hamadhussain@users.noreply.github.com>
2025-04-14 07:53:49 +0500

    Update page.jsx

--- src/app/Boxes/page.jsx
+++ src/app/Boxes/page.jsx
@@ -5,13 +5,13 @@ import Image from "next/image";
 
 const BoxGrid = () => {
   const boxes = [
-    { img: "/Boxes/logo1.png", text: "Sectors and Industries" },
-    { img: "/Boxes/logo2.png", text: "Business Description" },
-    { img: "/Boxes/logo3.png", text: "Locations" },
-    { img: "/Boxes/logo4.png", text: "Business Cycles and Backing Status" },
-    { img: "/Boxes/logo5.png", text: "Deals and Investors" },
-    { img: "/Boxes/logo6.png", text: "Financial Metrics" },
-    { img: "/Boxes/logo7.png", text: "Intellectual Property" },
+    { img: "/Boxes/LOGO1.png", text: "Sectors and Industries" },
+    { img: "/Boxes/LOGO2.png", text: "Business Description" },
+    { img: "/Boxes/LOGO3.png", text: "Locations" },
+    { img: "/Boxes/LOGO4.png", text: "Business Cycles and Backing Status" },
+    { img: "/Boxes/LOGO5.png", text: "Deals and Investors" },
+    { img: "/Boxes/LOGO6.png", text: "Financial Metrics" },
+    { img: "/Boxes/LOGO7.png", text: "Intellectual Property" },
   ];
 
   return (
@@ -22,9 +22,9 @@ const BoxGrid = () => {
         {boxes.map((box, index) => (
           <div key={index} className="box-grid-box">
             <p className="box-grid-text text-center px-2">{box.text}</p>
-            {index === 2 ? (
+{/*             {index === 2 ? (
               <IoEarth size={90} color="rgba(0, 0, 0, 1)" />
-            ) : (
+            ) : ( */}
               <Image
                 src={box.img}
                 alt={`Box ${index + 1}`}
@@ -32,7 +32,7 @@ const BoxGrid = () => {
                 width={100}
                 height={100}
               />
-            )}
+{/*             // )} */}
           </div>
         ))}
       </div>
